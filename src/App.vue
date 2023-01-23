0cca20e33cc3f845b7d5e99d5056a65ee28706a3
hecatris <74726881+Nils-Abjorn@users.noreply.github.com>
2023-01-23 14:32:48 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import "primevue/resources/primevue.min.css"; //core css
+import "primevue/resources/themes/lara-light-blue/theme.css"; //theme
+import "primeicons/primeicons.css";
+</script>
+
+<template>
+  <RouterView class="centered" />
+</template>
+
+<style lang="scss">
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <Card style="max-width: 400px; margin: auto">
+    <template #title> Cat fact </template>
+    <template #content>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+        consequuntur error repudiandae numquam deserunt quisquam repellat libero
+        asperiores earum nam nobis, culpa ratione quam perferendis esse,
+        cupiditate neque quas!
+      </p>
+    </template>
+    <template #footer>
+      <div class="centered">
+        <Button
+          icon="pi pi-refresh"
+          class="p-button-rounded p-button-primary"
+        />
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+import Card from "primevue/card";
+</script>
+
+<style lang="scss" scoped>
+.p-card-body {
+  display: flex;
+}
+
+.centered {
+  display: flex;
+  justify-content: center;
+}
+</style>
