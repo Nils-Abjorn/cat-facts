d00bf745e206a7eea496ea668525e91496bfa7c2
hecatris <74726881+Nils-Abjorn@users.noreply.github.com>
2023-01-24 10:20:18 +0100

    cleaning

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from "./App.vue";
-import router from "./router";
-import PrimeVue from "primevue/config";
+import App from "./App.vue"
+import router from "./router"
+import "@/styles"
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
-app.use(PrimeVue);
+app.use(createPinia())
+app.use(router)
 
-app.mount("#app");
+app.mount("#app")
---
+++ src/styles/index.ts
@@ -0,0 +1,2 @@
+import "element-plus/theme-chalk/dark/css-vars.css"
+import "./main.scss"
