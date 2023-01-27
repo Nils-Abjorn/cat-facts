fb6455b8f15d575566292a1a522f1c0bef6be2a0
Nils <74726881+Nils-Abjorn@users.noreply.github.com>
2023-01-27 12:40:57 +0100

    working tails, need ajustements

--- src/components/CatTail.vue
+++ src/components/CatTail.vue
@@ -1,44 +1,51 @@
 <template>
-  <div @click="initLine()">
-    <svg viewBox="0 0 10 10" class="svg-8">
-      <path :d="pathString" />
+  <div @click="path=initLine()">
+    <svg viewBox="0 0 10 10">
+      <path :d="pathString" :style="{transitionDuration: TRANSITION+'ms'}"/>
     </svg>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { useMousePressed } from "@vueuse/core"
+  import { useIntervalFn } from "@vueuse/core"
   import { computed, ref } from "vue"
 
+  const MAX_RANGE_ANGLE = 0.3; // rad 
+  const VERTEBRATE_LEN = 1.5; //remember that the viewbox is 10x10
+  const TRANSITION = 3000 //ms
+
+  interface Angle {
+    absolute: number
+      relative: number
+  }
   interface LinePoint {
     x: number
     y: number
-    angle: number
+    angle : Angle
   }
 
-  function calcRandomAngle(previousAngle: number) {
-    const angle = previousAngle + Math.random() * 1.4 - 0.7
-    return angle
+  function calcRandomAngle(previousAngle: Angle): Angle {
+    const relative = Math.random() * MAX_RANGE_ANGLE - MAX_RANGE_ANGLE/2
+    const absolute = previousAngle.absolute + relative
+    return { absolute, relative }
   }
 
   function calcLinePoint(previousLinePoint: LinePoint) {
-    console.log(previousLinePoint.angle)
-    const point = {
-      x: previousLinePoint.x + 2 * Math.cos(previousLinePoint.angle),
-      y: previousLinePoint.y - 2 * Math.sin(previousLinePoint.angle),
+    const point: LinePoint = {
+      x: previousLinePoint.x + VERTEBRATE_LEN * Math.cos(previousLinePoint.angle.absolute),
+      y: previousLinePoint.y - VERTEBRATE_LEN * Math.sin(previousLinePoint.angle.absolute),
       angle: calcRandomAngle(previousLinePoint.angle),
     }
-    console.log(point)
     return point
   }
 
-  function initLine(totalPoints = 4): LinePoint[] {
+  function initLine(curveModifier: number, totalPoints = 4): LinePoint[] {
     const line: LinePoint[] = []
 
     const lineOrigin: LinePoint = {
       x: 5,
       y: 10,
-      angle: calcRandomAngle(Math.PI / 2),
+      angle: calcRandomAngle({absolute: Math.PI / 2,relative: 0}),
     }
     line.push(lineOrigin)
     for (let i = 1; i < totalPoints; i++) {
@@ -46,8 +53,11 @@
     }
     return line
   }
-
-  const path = ref(initLine())
+  const curveModifier = Math.random() * MAX_RANGE_ANGLE - MAX_RANGE_ANGLE/2
+  const path = ref(initLine(curveModifier))
+  useIntervalFn(() => {
+    path.value = initLine(curveModifier)
+}, TRANSITION)
   const pathString = computed(() => {
     let str = `M${path.value[0].x},${path.value[0].y}`
     for (let i = 1; i < path.value.length; i++) {
@@ -55,23 +65,22 @@
     }
     return str
   })
-
-  const { pressed } = useMousePressed()
 </script>
 
 <style scoped lang="scss">
   svg {
-    width: 200%;
-    height: 200%;
+    width: 100%;
+    height: 100%;
     background: none;
+    stroke-width: 0.7;
   }
-  svg polyline,
-  svg line,
-  svg path {
+  
+  path {
     fill: none;
     stroke: var(--el-text-color-primary);
     stroke-linecap: round;
     stroke-linejoin: round;
-    transition: 0.5s;
+    transition-timing-function: cubic-bezier(0.3, 0, 0.7, 1);
   }
+
 </style>
