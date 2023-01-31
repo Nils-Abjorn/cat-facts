7c7810ab23bb6aaa471ac0486ada6306395af5e9
hecatris <74726881+Nils-Abjorn@users.noreply.github.com>
2023-01-31 10:31:04 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
-  <TailHandler ref="tailHandlerRef" />
-  <FactCard @newFact="addTail()" />
+  <!-- <TailHandler ref="tailHandlerRef" /> -->
+  <FactCard />
 </template>
 
 <script setup lang="ts">
   import FactCard from "@/components/FactCard.vue"
-  import TailHandler from "@/components/TailHandler.vue"
+  // import TailHandler from "@/components/TailHandler.vue"
   import { ref } from "vue"
 
   const tailHandlerRef = ref()
 
-  function addTail() {
-    tailHandlerRef.value.addTail()
-  }
+  // function addTail() {
+  //   tailHandlerRef.value.addTail()
+  // }
 </script>
